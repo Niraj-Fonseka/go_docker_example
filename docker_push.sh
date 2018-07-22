ae01017cc30bccc01841ed9b1a123f691c4d4cd1
NirajFonseka <fonseka.live@utexas.edu>
2018-07-22 00:48:09 -0500

    Testing the curl command in script

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p  "$DOCKER_PASSWORD"
 docker push dockerfonseka/dockergoapptwo
+
+
+
+echo "---- Post to the Deployment Manager ----"
+
+curl http://www.google.com
